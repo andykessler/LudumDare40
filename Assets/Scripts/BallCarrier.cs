9dd7fea2fdec0d834a9e65b18db1f3649bfe157a
Andy Kessler <akkessler@users.noreply.github.com>
2017-12-04 19:24:01 -0600

    One serving of spaghetti

--- Assets/Scripts/BallCarrier.cs
+++ Assets/Scripts/BallCarrier.cs
@@ -14,15 +14,12 @@ public class BallCarrier : MonoBehaviour {
 
     public Quaternion spawnQuaternion;
 
+    public int currentLives; // add maxlives var?
+
 	// Use this for initialization
 	void Start () {
         spawnPosition = transform.position;
         spawnQuaternion = transform.rotation;
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
 	}
 
     void ThrowBall(BallCarrier target)
@@ -55,7 +52,19 @@ public class BallCarrier : MonoBehaviour {
     void Kill()
     {
         Debug.Log("Carrier killed.");
+        GameLoop.KillAndRespawnIfHaveLife(this); // call this after short delay.
+        // remove 1 from life
+        // Call death animation/death script, aftwerwards...
+        // hide carrier from display until we respawn
+        // if remaining lives is greater than 0, respawn after certain amount of time
+
+    }
+
+    public void Respawn()
+    {
+        Debug.Log("Respawning Character");
         transform.SetPositionAndRotation(spawnPosition, spawnQuaternion);
+        // show carrier once set in position
     }
 
     public bool HasBall()
