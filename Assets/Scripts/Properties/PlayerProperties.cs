cc6ede22097ce510591e7477a3edd70de92d7ac0
Andy Kessler <akkessler@users.noreply.github.com>
2017-12-05 22:49:43 -0600

    Create property files, prototype some UI components

---
+++ Assets/Scripts/Properties/PlayerProperties.cs
@@ -0,0 +1,25 @@
+﻿public class PlayerProperties : UnitProperties
+{
+    public static PlayerProperties props = new PlayerProperties();
+
+    public const string KEY_SPEED = "speed";
+    public const string KEY_MAX_SPEED = "maxSpeed";
+    public const string KEY_MASS = "mass";
+    public const string KEY_ROTATION_SPEED = "rotationSpeed";
+    public const string KEY_THROW_STRENGTH = "throwStrength";
+
+    public static float speed;
+    public static float maxSpeed;
+    public static float mass;
+    public static float rotationSpeed;
+    public static float throwStrength;
+
+    protected PlayerProperties() : base()
+    {
+        d[KEY_SPEED] = 16f;
+        d[KEY_MAX_SPEED] = 16f;
+        d[KEY_MASS] = 2f;
+        d[KEY_ROTATION_SPEED] = 0.3f;
+        d[KEY_THROW_STRENGTH] = 200f;
+    }
+}
---
+++ Assets/Scripts/Properties/UI/MinMaxSlider.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class MinMaxSlider : MonoBehaviour {
+    
+    public float min;
+    public float max;
+
+    public EventTrigger.TriggerEvent customCallback;
+
+    private Slider minSlider, maxSlider;
+
+    private Text textRange;
+
+    // Use this for initialization
+    void Start () {
+        Slider[] s = GetComponentsInChildren<Slider>();
+        if (s.Length != 2) throw new MissingComponentException();
+
+        minSlider = s[0];
+        maxSlider = s[1];
+
+        minSlider.minValue = min;
+        minSlider.maxValue = max;
+
+        maxSlider.minValue = min;
+        maxSlider.maxValue = max;
+
+        minSlider.value = min;
+        maxSlider.value = max;
+    }
+
+    // this is a callback for us to know to alter settings files;
+    public void HasChanged()
+    {
+        if(maxSlider.value < minSlider.value)
+        {
+            Slider temp = maxSlider;
+            maxSlider = minSlider;
+            minSlider = temp;
+        }
+        
+        textRange.text = String.Format("{0}: {1} - {2}", transform.parent.name, minSlider.value, maxSlider.value);
+
+        BaseEventData eventData = new BaseEventData(EventSystem.current);
+        eventData.selectedObject = gameObject;
+        customCallback.Invoke(eventData);
+    }
+}
